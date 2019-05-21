ed3cf0f7b039e13d032fffcd4d5c92818c06993b
WhosKnee <husnifareed9@gmail.com>
2019-05-20 22:23:22 -0400

    changed port for heroku server

--- app.js
+++ app.js
@@ -84,13 +84,6 @@ app.use(indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments",commentRoutes);
 
-
-app.listen(3000, function(){
-    console.log("Yelpcamp has started")
-})
-
-/*
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("The YelpCamp Server has Started");
 })
-*/
